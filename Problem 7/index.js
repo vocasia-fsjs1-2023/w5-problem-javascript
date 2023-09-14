2e9b9aeb71ece90a9786e86918434524e7fb50bb
juasruhiyat15 <juasruhiyat567@gmail.com>
2023-09-14 17:01:11 +0800

    Finish Problem 7

---
+++ Problem 7/index.js
@@ -0,0 +1,42 @@
+// Tugas 7.1 (Done)
+
+for(let i=1; i<=100; i++){
+    if(i%6 === 1){
+        console.log(i + ' Ganjil');
+        continue;
+    }else if(i%3 === 1){
+        console.log(i+ ' Genap!');
+        continue;
+    }
+    
+}
+// Tugas 7.1 (Done)
+
+
+
+// Tugas 7.2 (Done)
+
+
+for(let j=50;j<=200;j+=5){
+    if(j%3 === 0){
+        console.log(j + ' faktor 3');
+    }else {
+        console.log(j + ' Tidak Bisa di Bagi 3');
+    }
+}
+
+// Tugas 7.2 (Done)
+
+
+
+// Tugas 7.3 (Done)
+
+for(let k=100; k<=200;k++){
+    if(k%8===0 && k%7===2){
+        console.log(k);
+    }
+}
+
+// Tugas 7.3 (Done)
+
+
