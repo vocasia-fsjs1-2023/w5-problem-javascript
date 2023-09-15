227d01b3086461daf5676103d367e71af9544e05
fahriwijayaa <fahriw44@gmail.com>
2023-09-15 21:58:48 +0700

    Pengumpulan Tugas Week 5

--- Problem 5/index.js
+++ Problem 5/index.js
@@ -7,16 +7,18 @@ var fifth = "I";
 var sixth = "love";
 var seventh = "it!";
 
-console.log("Problem 3-1", "<lakukan disini>");
+console.log("Problem 3-1")
+console.log(word + ' ' + second + ' ' + third + ' ' + fourth + ' ' + sixth + ' ' + seventh);
 
 // Problem 3-2
 var word2 = "wow JavaScript is so cool";
 var exampleFirstWord = word2[0] + word2[1] + word2[2];
-var secondWord; // do your own!
-var thirdWord; // do your own!
-var fourthWord; // do your own!
-var fifthWord; // do your own!
+var secondWord = word2[4] + word2[5] + word2[6] + word2[7] + word2[8] + word2[9] + word2[10] + word2[11] + word2[12] + word2[13]; // do your own!
+var thirdWord = word2[15] + word2[16]; // do your own!
+var fourthWord = word2[18] + word2[19]; // do your own!
+var fifthWord = word2[21] + word2[22] + word2[23] + word2[24]; // do your own!
 
+console.log("\nProblem 3-2")
 console.log("First Word: " + exampleFirstWord);
 console.log("Second Word: " + secondWord);
 console.log("Third Word: " + thirdWord);
@@ -26,11 +28,12 @@ console.log("Fifth Word: " + fifthWord);
 // Problem 3-3
 var word3 = "wow JavaScript is so cool";
 var exampleFirstWord3 = word3.substring(0, 3);
-var secondWord3; // do your own!
-var thirdWord3; // do your own!
-var fourthWord3; // do your own!
-var fifthWord3; // do your own!
+var secondWord3 = word3.substring(4, 14); // do your own!
+var thirdWord3 = word3.substring(15, 17); // do your own!
+var fourthWord3 = word3.substring(18, 20); // do your own!
+var fifthWord3 = word3.substring(21, 25); // do your own!
 
+console.log("\nProblem 3-3")
 console.log("First Word: " + exampleFirstWord3);
 console.log("Second Word: " + secondWord3);
 console.log("Third Word: " + thirdWord3);
@@ -40,18 +43,21 @@ console.log("Fifth Word: " + fifthWord3);
 // Problem 3-4
 var word4 = "wow JavaScript is so cool";
 var exampleFirstWord4 = word4.substring(0, 3);
-var secondWord4; // do your own!
-var thirdWord4; // do your own!
-var fourthWord4; // do your own!
-var fifthWord4; // do your own!
+var secondWord4 = word4.substring(4, 14); // do your own!
+var thirdWord4 = word4.substring(15, 17); // do your own!
+var fourthWord4 = word4.substring(18, 20); // do your own!
+var fifthWord4 = word4.substring(21, 25); // do your own!
 
 var firstWordLength = exampleFirstWord4.length;
 // create new variables around here
+var secondWord4Lenght = secondWord4.length;
+var thirdWord4Lenght = thirdWord4.length;
+var fourthWord4Lenght = fourthWord4.length;
+var fifthWord4Lenght = fifthWord4.length;
 
-console.log(
-  "First Word: " + exampleFirstWord4 + ", with length: " + firstWordLength
-);
-console.log("Second Word: " + secondWord4);
-console.log("Third Word: " + thirdWord4);
-console.log("Fourth Word: " + fourthWord4);
-console.log("Fifth Word: " + fifthWord4);
+console.log("\nProblem 3-4")
+console.log("First Word: " + exampleFirstWord4 + ", with length: " + firstWordLength);
+console.log("Second Word: " + secondWord4 + ", with length: " + secondWord4Lenght);
+console.log("Third Word: " + thirdWord4 + ", with length: " + thirdWord4Lenght);
+console.log("Fourth Word: " + fourthWord4 + ", with length: " + fourthWord4Lenght);
+console.log("Fifth Word: " + fifthWord4 + ", with length: " + fifthWord4Lenght);
