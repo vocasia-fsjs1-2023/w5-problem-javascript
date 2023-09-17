c1dd1632b9ba0630846494e69efc3222340b0965
evaald <evaaldori239@gmail.com>
2023-09-17 19:30:43 +0700

    Problem 7 Eva

---
+++ Problem 7/index.js
@@ -0,0 +1,24 @@
+// problem 1
+for(let number = 1; number<101; number+=3){
+    if(number%2 === 0){
+        console.log(number+"-genap")
+    }else{
+        console.log(number+"-ganjil")
+    }
+  }
+
+  //problem 2
+for(let number = 50; number<201; number+=5){
+    if(number%3 === 0){
+        console.log(number+"-faktor 3")
+    }else{
+        console.log(number+"-tidak bisa dibagi 3")
+    }
+  }
+
+  //problem 3
+for(let number = 100; number<201; number+=7){
+    if(number%8 === 0){
+        console.log(number)
+    }
+  }
