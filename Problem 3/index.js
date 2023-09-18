b284d13bc477c5a2fe3898d755b2e169aff23b2e
bimocahyo7 <bimochayo@gmail.com>
2023-09-18 22:16:16 +0800

    Tugas Minggu 5 JavaScript

--- Problem 3/index.js
+++ Problem 3/index.js
@@ -1,2 +1,21 @@
-var nama;
-var nilai;
+var nama = "Bimo Cahyo Kusumo";
+var nilai = 85;
+var grade = "";
+
+if (nilai >= 80 && nilai <= 100) {
+  grade = "A";
+} else if (nilai >= 65 && nilai <= 79) {
+  grade = "B";
+} else if (nilai >= 50 && nilai <= 64) {
+  grade = "C";
+} else if (nilai >= 35 && nilai <= 49) {
+  grade = "D";
+} else if (nilai >= 0 && nilai <= 34) {
+  grade = "E";
+} else if (nilai < 0 || nilai > 100) {
+  grade = "Nilai Invalid";
+}
+
+console.log("Nama : " + nama);
+console.log("Nilai: " + nilai);
+console.log("Grade: " + grade);
