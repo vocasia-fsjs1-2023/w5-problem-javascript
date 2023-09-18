9fb055e7a7859707b6d56320c799f1189469eb41
bimocahyo7 <bimochayo@gmail.com>
2023-09-18 22:18:33 +0800

    Tugas Minggu 5

---
+++ Problem 7/index.js
@@ -0,0 +1,30 @@
+//Problem 1
+for (i = 1; i <= 100; i += 3) {
+  if (i % 2 == 1) {
+    console.log(i + " - Ganjil");
+  } else {
+    console.log(i + " - Genap");
+  }
+}
+
+//Problem 2
+console.log("");
+
+for (i = 50; i <= 200; i += 5) {
+  if (i % 3 == 0) {
+    console.log(i + " - Faktor 3");
+  } else {
+    console.log(i + " - Tidak bisa dibagi 3");
+  }
+}
+
+//Problem 3
+console.log("");
+
+for (i = 100; i <= 200; i += 7) {
+  if (i % 8 == 0) {
+    console.log(i);
+  } else {
+    // console.log();
+  }
+}
