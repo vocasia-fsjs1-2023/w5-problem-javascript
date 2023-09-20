571f336880968a2ef0bfb444d52f265b6d856404
saidatunnizmah <aniskateha@gmail.com>
2023-09-20 23:57:07 +0800

    pengumpulan tugas week 5

--- Problem 3/index.js
+++ Problem 3/index.js
@@ -1,2 +1,22 @@
-var nama;
-var nilai;
+var nama = "Incun"
+var nilai= 90;
+if (nilai >= 0 && nilai <= 100) {
+  let desc;
+  if (nilai >= 80 && nilai <= 100) {
+    desc = "A";
+  } else if (nilai >= 65 && nilai < 80) {
+    desc = "B";
+  } else if (nilai >= 50 && nilai < 65) {
+    desc = "C";
+  } else if (nilai >= 35 && nilai < 50) {
+    desc = "D";
+  } else {
+    desc = "E";
+  }
+
+  console.log(`Nama: ${nama}`);
+  console.log(`Nilai: ${nilai}`);
+  console.log(`Deskripsi Nilai: ${desc}`);
+} else {
+  console.log("Nilai Invalid");
+}
---
+++ Problem 7/index.js
@@ -0,0 +1,25 @@
+// nomor 1
+console.log("Mengecek Ganjil/Genap dari 1 hingga 100:");
+for (var i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log(i + " - genap");
+  } else {
+    console.log(i + " - ganjil");
+  }
+}
+// nomor 2
+console.log("Mengecek Ganjil/Genap dari 1 hingga 100:");
+for (var i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log(i + " - genap");
+  } else {
+    console.log(i + " - ganjil");
+  }
+}
+// nomor 3
+console.log("\nMengecek Bilangan yang Habis Dibagi 8 dari 100 hingga 200:");
+for (var i = 100; i <= 200; i += 7) {
+  if (i % 8 === 0) {
+    console.log(i);
+  }
+}
