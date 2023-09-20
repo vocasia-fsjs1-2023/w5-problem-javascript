8ff7ee302e44729f7ff6887c32db29c4c61b57fe
anitasfyun <anitasfyunn@gmail.com>
2023-09-20 17:55:09 +0700

    pengumpulan_tugas_w5

--- Problem 3/index.js
+++ Problem 3/index.js
@@ -1,2 +1,25 @@
-var nama;
-var nilai;
+var nama = 'Anita';
+var nilai = 100;
+
+// Mengecek apakah nilai valid
+if (nilai < 0 || nilai > 100 || isNaN(nilai)) {
+    console.log("Nilai Invalid");
+} else {
+    
+// Menentukan nilai ujian berdasarkan rentang nilai
+let nilaiujian = "";
+if (nilai >= 80 && nilai <= 100) {
+    nilaiujian = "A";
+} else if (nilai >= 65 && nilai < 80) {
+    nilaiujian = "B";
+} else if (nilai >= 50 && nilai < 65) {
+    nilaiujian = "C";
+} else if (nilai >= 35 && nilai < 50) {
+    nilaiujian = "D";
+} else {
+    nilaiujian = "E";
+}
+
+// Menampilkan nilai ujian dan nama mahasiswa
+console.log(`Nilai Ujian ${nama} adalah : ${nilai} (${nilaiujian})`);
+}
---
+++ Problem 7/index.js
@@ -0,0 +1,28 @@
+// tugas 1
+for(let i=1; i<=100; i++) {
+    if(i%6 === 1) {
+        console.log(i + ' Ganjil');
+        continue;
+    }else if(i%3 === 1){
+        console.log(i+ ' Genap!');
+        continue;
+    }
+} 
+
+
+// tugas 2
+for(let j=50;j<=200;j+=5) {
+    if(j%3 === 0) {
+        console.log(j + ' faktor 3');
+    }else {
+        console.log(j + ' Tidak Bisa di Bagi 3');
+    }
+}
+
+
+// tugas 3
+for(let k=100; k<=200;k++){
+    if(k%8===0 && k%7===2){
+        console.log(k);
+    }
+}
