7cdf09b966f9aec9845e53968a3c2862a66e478c
dhiyaulhaqm <xwy.dib2020@gmail.com>
2023-09-27 01:32:21 +0800

    problem 2 done

--- Problem 2/index.js
+++ Problem 2/index.js
@@ -1,25 +1,15 @@
-let name = "Dhiya";
-let age = 16;
-let money =  '400000';
+let name = "Dhiyaa";
+let age = 19;
+let money = 50000;
 
 if (name === "") {
   console.log("Anda tidak boleh masuk!");
+} else if (age < 17 && money >= 50000) {
+  money -= 50000;
+  console.log(`Anda bisa pesan juice. Sisa uang Anda: ${money}`);
+} else if (age >= 17 && money >= 30000) {
+  money -= 30000;
+  console.log(`Anda bisa pesan anggur. Sisa uang Anda: ${money}`);
 } else {
-  if (age < 17) {
-    if (money >= 50000) {
-      money -= 50000;
-      console.log('Anda bisa pesan juice. Sisa uang Anda:');
-      console.log( money );
-    } else {
-      console.log("Uang tidak cukup. Anda harus pulang.");
-    }
-  } else {
-    if (money >= 300000) {
-      money -= 300000; 
-      console.log('Anda bisa pesan anggur. Sisa uang Anda:');
-      console.log( money );
-    } else {
-      console.log("Uang tidak cukup. Anda harus pulang.");
-    }
-  }
+  console.log("Uang tidak cukup. Anda harus pulang.");
 }
