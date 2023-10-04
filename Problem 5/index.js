7cbd6ac2d3471f3d265b3b813f2a2cc06b577b0f
SeptiaAriandini30 <septiaariandini@gmail.com>
2023-10-04 08:33:19 +0700

    tugas w5

--- Problem 5/index.js
+++ Problem 5/index.js
@@ -1,4 +1,3 @@
-// Problem 3-1
 var word = "JavaScript";
 var second = "is";
 var third = "awesome";
@@ -6,52 +5,47 @@ var fourth = "and";
 var fifth = "I";
 var sixth = "love";
 var seventh = "it!";
-
-console.log("Problem 3-1", "<lakukan disini>");
-
-// Problem 3-2
+var sentence = word + " " + second + " " + third + " " + fourth + " " + fifth + " " + sixth + " " + seventh;
+console.log(sentence);
 var word2 = "wow JavaScript is so cool";
 var exampleFirstWord = word2[0] + word2[1] + word2[2];
-var secondWord; // do your own!
-var thirdWord; // do your own!
-var fourthWord; // do your own!
-var fifthWord; // do your own!
+var secondWord = word2.substring(4, 14);
+var thirdWord = word2.substring(15, 17);
+var fourthWord = word2.substring(18, 20);
+var fifthWord = word2.substring(21, 24);
 
 console.log("First Word: " + exampleFirstWord);
 console.log("Second Word: " + secondWord);
 console.log("Third Word: " + thirdWord);
 console.log("Fourth Word: " + fourthWord);
 console.log("Fifth Word: " + fifthWord);
-
-// Problem 3-3
 var word3 = "wow JavaScript is so cool";
 var exampleFirstWord3 = word3.substring(0, 3);
-var secondWord3; // do your own!
-var thirdWord3; // do your own!
-var fourthWord3; // do your own!
-var fifthWord3; // do your own!
+var secondWord3 = word3.substring(4, 14);
+var thirdWord3 = word3.substring(15, 17);
+var fourthWord3 = word3.substring(18, 20);
+var fifthWord3 = word3.substring(21, 24);
 
 console.log("First Word: " + exampleFirstWord3);
 console.log("Second Word: " + secondWord3);
 console.log("Third Word: " + thirdWord3);
 console.log("Fourth Word: " + fourthWord3);
 console.log("Fifth Word: " + fifthWord3);
-
-// Problem 3-4
 var word4 = "wow JavaScript is so cool";
 var exampleFirstWord4 = word4.substring(0, 3);
-var secondWord4; // do your own!
-var thirdWord4; // do your own!
-var fourthWord4; // do your own!
-var fifthWord4; // do your own!
+var secondWord4 = word4.substring(4, 14);
+var thirdWord4 = word4.substring(15, 17);
+var fourthWord4 = word4.substring(18, 20);
+var fifthWord4 = word4.substring(21, 24);
 
 var firstWordLength = exampleFirstWord4.length;
-// create new variables around here
+var secondWordLength = secondWord4.length;
+var thirdWordLength = thirdWord4.length;
+var fourthWordLength = fourthWord4.length;
+var fifthWordLength = fifthWord4.length;
 
-console.log(
-  "First Word: " + exampleFirstWord4 + ", with length: " + firstWordLength
-);
-console.log("Second Word: " + secondWord4);
-console.log("Third Word: " + thirdWord4);
-console.log("Fourth Word: " + fourthWord4);
-console.log("Fifth Word: " + fifthWord4);
+console.log("First Word: " + exampleFirstWord4 + ", with length: " + firstWordLength);
+console.log("Second Word: " + secondWord4 + ", with length: " + secondWordLength);
+console.log("Third Word: " + thirdWord4 + ", with length: " + thirdWordLength);
+console.log("Fourth Word: " + fourthWord4 + ", with length: " + fourthWordLength);
+console.log("Fifth Word: " + fifthWord4 + ", with length: " + fifthWordLength);
