779876398973763ea585597f816f71363348dbf2
dhiyaulhaqm <xwy.dib2020@gmail.com>
2023-10-19 14:14:46 +0800

    problem 4 done

--- Problem 5/index.js
+++ Problem 5/index.js
@@ -7,15 +7,15 @@ var fifth = "I";
 var sixth = "love";
 var seventh = "it!";
 
-console.log("Problem 3-1", "<lakukan disini>");
+console.log("Problem 3-1", " ", word + " " + second + " " + third + " " + fourth + " " + fifth + " " + sixth + " " + seventh);
 
 // Problem 3-2
 var word2 = "wow JavaScript is so cool";
 var exampleFirstWord = word2[0] + word2[1] + word2[2];
-var secondWord; // do your own!
-var thirdWord; // do your own!
-var fourthWord; // do your own!
-var fifthWord; // do your own!
+var secondWord = word2[4] + word2[5] + word2[6] + word2[7] + word2[8] + word2[9] + word2[10] + word2[11] + word2[13];
+var thirdWord = word2[15] + word2[16];
+var fourthWord = word2[18] + word2[19];
+var fifthWord = word2[21] + word2[22] + word2[23] + word2[24];
 
 console.log("First Word: " + exampleFirstWord);
 console.log("Second Word: " + secondWord);
@@ -26,10 +26,10 @@ console.log("Fifth Word: " + fifthWord);
 // Problem 3-3
 var word3 = "wow JavaScript is so cool";
 var exampleFirstWord3 = word3.substring(0, 3);
-var secondWord3; // do your own!
-var thirdWord3; // do your own!
-var fourthWord3; // do your own!
-var fifthWord3; // do your own!
+var secondWord3 = word3.substring(4, 14);
+var thirdWord3 = word3.substring(15, 17);
+var fourthWord3 = word3.substring(18, 20);
+var fifthWord3 = word3.substring(21, 25);
 
 console.log("First Word: " + exampleFirstWord3);
 console.log("Second Word: " + secondWord3);
@@ -40,13 +40,17 @@ console.log("Fifth Word: " + fifthWord3);
 // Problem 3-4
 var word4 = "wow JavaScript is so cool";
 var exampleFirstWord4 = word4.substring(0, 3);
-var secondWord4; // do your own!
-var thirdWord4; // do your own!
-var fourthWord4; // do your own!
-var fifthWord4; // do your own!
+var secondWord4 = word4.substring(4, 14);
+var thirdWord4 = word4.substring(15, 17);
+var fourthWord4 = word4.substring(18, 20);
+var fifthWord4 = word4.substring(21, 25);
 
 var firstWordLength = exampleFirstWord4.length;
 // create new variables around here
+var secondWordLength = secondWord4.length;
+var thirdWordLength = thirdWord3.length;
+var fourthWordLength = fourthWord3.length;
+var fifthWordLength = fifthWord4.length;
 
 console.log(
   "First Word: " + exampleFirstWord4 + ", with length: " + firstWordLength
--- Problem 6/index.js
+++ Problem 6/index.js
@@ -1,6 +1,18 @@
 var kata = "I love Javascript";
 
 // Do your work here
+var hasil = '';
 
+for (var i = 0; i < kata.length; i++) {
+  var huruf = kata[i];
+  if (huruf === 'a' || huruf === 'e' || huruf === 'i' || huruf === 'o' || huruf === 'u') {
+    hasil += '$';
+  } else {
+    hasil += huruf;
+  }
+
+  hasil = kata;
+}
 // Dont work after this line
 console.log(kata);
+
